520d7071c4587118a3d9f771a4cea33e5127f367
Aydin Senturk <hello@aydinsenturk.com>
2020-04-28 20:37:58 +0300

    eslint-config-base added

---
+++ packages/eslint-config-base/README.md
@@ -0,0 +1,11 @@
+# `@startrco/eslint-config-base`
+
+> TODO: description
+
+## Usage
+
+```
+const eslintConfigBase = require('@startrco/eslint-config-base');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/eslint-config-base/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+    'plugin:eslint-comments/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  env: {
+    jest: true,
+  },
+  plugins: ['import', 'prettier', 'jest', 'eslint-comments'],
+  rules: {
+    'prettier/prettier': ['error'],
+  },
+};
